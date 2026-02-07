0ec14601c4f14f2ca610e72e477a585133bf26b0
yvarava-ai <yvarava@gmail.com>
2026-02-07 18:58:57 +0200

    search в - м

--- reg01.py
+++ reg01.py
@@ -1,10 +1,8 @@
 import re
 
 text = "Вивчення Python може бути веселим."
-pattern = "Python"
-match = re.search(pattern, text)
+pattern = r"в\w*м"  # пошук слова, що починається на "в" та закінчується на "м"
+match = re.search(pattern, text, re.IGNORECASE)
 
 if match:
-    print("Знайдено:", match.span())
-else:
-    print("Не знайдено.")
+    print("Знайдено:", match.group())
